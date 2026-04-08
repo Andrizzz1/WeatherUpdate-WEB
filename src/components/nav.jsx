31528601f474e4f7ee6144edd2d459812a46d8e7
Andrizzz1 <andreimandapat09@gmail.com>
2026-04-08 21:48:18 +0800

    COMPLETE WEBCARD

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,13 +1,13 @@
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline'
 import { MapPinIcon } from '@heroicons/react/24/outline'
 function NavBar({city, setCity, onSearch}){
-    return<nav className='flex justify-around p-4'>
-            <p>Weather Update</p>
-            <div className='flex bg-gray-300 rounded-2xl gap-1 p-2'>
+    return<nav className='flex justify-around p-4 max-md:justify-between max-sm:justify-center gap-10 '>
+            <p className='font-semibold uppercase max-sm:text-sm'>Weather <br className='sm:hidden' /> Update</p>
+            <div className='flex justify-between bg-gray-300 rounded-2xl gap-1 p-2 w-5/12 max-md:p-1 '>
                 <input 
                     value={city}
                     onChange={(e)=>{setCity(e.target.value)}}
-                    className='outline-0 w-xl' 
+                    className='outline-0 w-full min-w-0' 
                     type="text" 
                     placeholder="Search for a city"/>
                 <button onClick={onSearch} className='cursor-pointer'>
@@ -15,7 +15,9 @@ function NavBar({city, setCity, onSearch}){
                 </button>
                 
             </div>
-            <MapPinIcon className="w-6 cursor-pointer" />
+            <div className='flex items-center shrink-0'>
+                <MapPinIcon className="w-6 cursor-pointer" />
+            </div>
         </nav>
 }
 
--- src/components/webcard.jsx
+++ src/components/webcard.jsx
@@ -1,20 +1,27 @@
-
-
 function Webcard({weather}){
     if (!weather) return <p>No Weather Data</p>
     const today = new Date();
-    return<div className="bg-[linear-gradient(180deg,#1d8cf8_0%,#a4d8ff_100%)] w-5/12 h-90 rounded-2xl mt-5" >
-        <div>
+    return<div className="bg-[linear-gradient(180deg,#1d8cf8_0%,#a4d8ff_100%)] w-5/12 max-lg:w-6/12 max-md:w-8/12 max-sm:w-9/12  h-90 rounded-2xl mt-5" >
+        <div className="flex justify-between text-white p-3">
             <div >
-                <p id="CityName">{weather.cityName}</p>
-                <p className="text-sm" id="date">{today.toLocaleDateString("en-US",{weekday:"short"})}, 
-                                {today.toLocaleDateString("en-US",{month:"short"})}</p>
+                <p className="text-2xl font-semibold" id="CityName">{weather.cityName}</p>
+                <p className="text-sm" id="date">{today.toLocaleDateString("en-US",{weekday:"short"})},{" "} 
+                                {today.toLocaleDateString("en-US",{month:"short"})}{" "}
+                                {today.getDate()}{" "}
+                                {today.toLocaleTimeString("en-US",{hour:"numeric",minute:"2-digit",hour12:true})}</p>
+            </div>
+            <div className="text-sm flex items-center bg-white/20 backdrop-blur-md p-2 rounded-3xl">
+                <img className="w-10" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt="" />
+                {weather.weather[0].description}
             </div>
         </div>
-        <div>
-            <h1 id="Temp">{weather.main.temp}</h1>
-            <div>
-                <p id="skyLabel"></p>
+        <div className="text-white grid grid-cols-2 items-center">
+            <div className="flex justify-center items-center h-64">
+                <h1 id="Temp" className="text-8xl max-lg:text-7xl max-sm:text-6xl font-semibold">{Math.round(weather.main.temp)}°C</h1>
+            </div>
+            <div className="">
+                <p id="skyLabel" className="text-2xl max-sm:text-xl font-semibold">{weather.weather[0].main}</p>
+                <p className="text-sm">High {weather.main.temp_max}° • Low {weather.main.temp_min}°</p>
             </div>
         </div>
     </div>
